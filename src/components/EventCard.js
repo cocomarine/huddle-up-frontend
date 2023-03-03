2d54544e64e3cb847e0ef07a12cffc1f215554a9
HJ Kang <hkang07@gmail.com>
2023-03-03 13:53:44 +0000

    my events page updated

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,4 +1,8 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+import "../styles/event-card.css";
 
 const EventCard = ({
   id,
@@ -7,11 +11,47 @@ const EventCard = ({
   voting_finished,
   AdminId,
 }) => {
-  
+
+  const [adminFirstName, setAdminFirstName] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/events/${id}`)
+      .then((res) => {
+        const adminID = res.data.AdminId;
+        const eventUsers = res.data.Users;
+        const adminData = eventUsers.filter(
+          (eventUser) => eventUser.id === adminID
+        );
+
+        setAdminFirstName(adminData[0].first_name);
+        setSuggestions(res.data.Suggestions);
+      })
+  }, []);
+
   return (
-    <div className="voting-ended-container">
+    <div className="event-card">
+      <div className="event-card-container">
+      <button 
+          className="event-card__close-button"
+          onClick={() => {}}
+        >
+          <IoIosCloseCircleOutline />
+        </button>
+        <div className="event-card__title">{title}</div>
+        <div className="event-card__description">{description}</div>
+        <div className="event-card__suggestions">
+          {/* {suggestions && suggestions.map((suggestion) => {
+            <div className="voted-cards__item" key={suggestion.id}>
+              {suggestion}
+            </div>
+          })} */}
+        </div>
+        <div className="event-card__admin">Creater: {adminFirstName}</div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default EventCard;
--- src/components/MyEvents.js
+++ src/components/MyEvents.js
@@ -1,45 +1,75 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll-v2";
+import axios from "axios";
 import EventCard from "./EventCard";
 import VotedEventCard from "./VotedEventCard";
-import "../styles/myevents.css";
+import "../styles/my-events.css";
 
 const MyEvents = () => {
-  const navigate = useNavigate();
+  const initialState = {
+    votedEvents: [],
+    pendingEvents: [],
+  };
+
+  const [votedEvents, setVotedEvents] = useState(initialState.votedEvents);
+  const [pendingEvents, setPendingEvents] = useState(initialState.pendingEvents);
+  const [alert, setAlert] = useState({ message: "" });
+  // const [cardRemoved, setCardRemoved] = useState(false);
+
+  const getVotedEvents = (event) => {
+    const totalUsers = event.Users.length;
+    const suggestionsList = event.Suggestions;
+    const totalVotes = suggestionsList.reduce(
+      (prev, current) => prev + current.votes, 0
+    );
+    return totalUsers - totalVotes;
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/events")
+      .then((res) => {
+        setVotedEvents(res.data.filter((event) => getVotedEvents(event) === 0));
+        setPendingEvents(res.data.filter((event) => getVotedEvents(event) > 0));
+      })
+      .catch(() => {
+        setAlert({
+          message: "Server error. Please try again later.",
+        });
+      });
+  }, []);
+
   return (
-    <div>
-      <h4> My Current Events</h4>
-      <div>
-        <button onClick={navigate("/myevents")} type="submit">
-          Join an event
-        </button>
-        <div>
-          <button onClick={navigate("/myevents")} type="submit">
-            Voting finished: 1
-          </button>
-        </div>
-        <div>
-          <button onClick={navigate("/myevents")} type="submit">
-            Voting in progress : 1
-          </button>
-        </div>
-        <div>
-          <button onClick={navigate("/myevents")} type="submit">
-            Suggestions in progress: 1
-          </button>
-        </div>
-        <div>
-          <button onClick={navigate("/myevents")} type="submit">
-            Create a new event
-          </button>
+    <div className="events">
+      <h3> My Events</h3>
+      <div className="navigate-events">
+        <AnchorLink href='#event-cards-voted'>
+          <button>Voting finished</button>
+        </AnchorLink>
+        <AnchorLink href='#event-cards-pending'>
+          <button>Voting in progress</button>
+        </AnchorLink>
+        <div className="join-create-buttons">
+          <button>Join an event</button>
+          <button>Create an event</button>
         </div>
       </div>
-      <div className="events-cards">
-        <div className="events-cards-voted">
-          <VotedEventCard />
+      <div className="event-cards">
+        <div className="event-cards-voted" id="event-cards-voted">
+          <h4>Voting Finished</h4>
+          {votedEvents && votedEvents.map((votedEvent) => (
+            <div className="voted-cards__item" key={votedEvent.id}>
+              <VotedEventCard {...votedEvent} />
+            </div>
+          ))}
         </div>
-        <div className="events-cards-voting">
-          <EventCard />
+        <div className="event-cards-pending" id="event-cards-pending">
+          <h4>Voting In Progress</h4>
+          {pendingEvents && pendingEvents.map((pendingEvent) => (
+            <div className="event-cards__item" key={pendingEvent.id}>
+              <EventCard {...pendingEvent} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
