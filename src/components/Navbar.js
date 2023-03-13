43a0039a89f3e7c0675d8b3c1cf7a6ec06ad48f7
Rayhan Elbeera <raybeera2014@gmail.com>
2023-03-13 09:37:18 +0000

    navbar updated with UserAuth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { MdReorder } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { ImCross } from "react-icons/im";
+import { useAuthContext } from "../hooks/useAuthContext";
 import "../styles/navbar.css";
 import logo from "../img/AdobeStock_375037420.jpeg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-
+  const { user } = useAuthContext();
   return (
     <div className="navbar">
       <div className="leftSide">
@@ -21,21 +22,21 @@ const Navbar = () => {
 
       <div className="rightSide">
         <div className="links" id={showLinks ? "hidden" : ""}>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/myprofile"> My Profile</Link>
-          <Link to="/signup">Sign up</Link>
-          <Link to="/joinevents"> Join Events</Link>
-          <Link to="/myevents"> My Events</Link>
+          {!user && <Link to="/login">Login</Link>}
+          {user && <Link to="/logout">Logout</Link>}
+          {user && <Link to="/myprofile"> My Profile</Link>}
+          {!user && <Link to="/signup">Sign up</Link>}
+          {user && <Link to="/joinevents"> Join Events</Link>}
+          {user && <Link to="/myevents"> My Events</Link>}
         </div>
       </div>
       <div className="icons">
-      <div className="closingIcon" onClick={() => setShowLinks(!showLinks)}>
-        {showLinks && <ImCross  />}
-      </div>
-      <div className="navIcon" onClick={() => setShowLinks(!showLinks)}>
-        {!showLinks && <MdReorder size="2.5rem" color="rgb(246, 73, 73)" />}
-      </div>
+        <div className="closingIcon" onClick={() => setShowLinks(!showLinks)}>
+          {showLinks && <ImCross />}
+        </div>
+        <div className="navIcon" onClick={() => setShowLinks(!showLinks)}>
+          {!showLinks && <MdReorder size="2.5rem" color="rgb(246, 73, 73)" />}
+        </div>
       </div>
     </div>
   );
