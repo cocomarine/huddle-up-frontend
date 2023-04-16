3087f38d888e55e8b2f6379d4d11319b023352e6
HJ Kang <hkang07@gmail.com>
2023-04-16 23:04:58 +0100

    refractored after reorg of files and props successfully passed

--- src/components/MapPage/Locations.js
+++ src/components/MapPage/Locations.js
@@ -1,132 +1,20 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useRef, useEffect } from "react";
+import React, { useEffect } from 'react';
 import "../../styles/locations.css";
 
-const Locations = (places) => {
-  const googleMapRef = useRef(null);
-  let googleMap = null;
+const Locations = ({locations}) => {
 
-  // ordering suggested places so that they keep their indices consistently throughout
-  // containing places details such as suggestion and place_id
-  const orderedSugList = Object.values(places)[0].sort((a, b) => {
-    if (a.suggestion < b.suggestion) {
-      return -1;
-    }
-    if (a.suggestion > b.suggestion) {
-      return 1;
-    }
-    return 0;
-  });
-
-  // From orderedSugList, make a list of place_id
-  const placeIdList = orderedSugList.map((suggestion) => suggestion.place_id);
-
-  const geocoder = new window.google.maps.Geocoder();
-  
-  // defining function for reverse geocodeing
-  const geocoderResult = (placeId) => {
-    return new Promise((resolve, reject) => {
-      geocoder
-        .geocode({ placeId: placeId }, (results, status) => {
-          if (status === window.google.maps.GeocoderStatus.OK) {
-            return resolve({
-              place_id: results[0].place_id,
-              lat: results[0].geometry.location.lat(),
-              lng: results[0].geometry.location.lng(),
-              address: results[0].formatted_address,
-            })
-          } else {
-            reject(new Error("No results found"));
-          }
-        });
-    });
-  };
-
-  // using above geocoderResult function, make a list of reverse geocoding results 
-  // containing locations and address (but not the name of place, need to be connected later using place_id)
-  const geocoderList= Promise.all(placeIdList.map((id) => geocoderResult(id).then(res => res)));
-
-  let markerList = [];
-
-  // const labelList = {
-  //   label1: "1",
-  //   label2: "2",
-  // }
-
-  // const markerList = [
-  //   { lat: 53.52083, lng: -2.30332, label: labelList.label1 },
-  //   { lat: 53.48083, lng: -2.14332, label: labelList.label2 }
-  // ]
-
-  // initialising map
-  const initGoogleMap = () => {
-    return new window.google.maps.Map(googleMapRef.current, {
-      zoom: 11, 
-      center: {
-        // Manchester city centre
-        lat: 53.48083,
-        lng: -2.24332
-      },
-      disableDefaultUI: true,
-    });
-  }
-
-  // adding marker on google map
-  const addMarker = (markerObj) => new window.google.maps.Marker({
-    position: { lat: markerObj.lat, lng: markerObj.lng },
-    label: markerObj.label,
-    map: googleMap,
-  });
-
-
-  useEffect(() => {
-    googleMap = initGoogleMap();
-    
-    let bounds = new window.google.maps.LatLngBounds();
-
-    geocoderList.then((res) => {
-      // map method with callback function for selcting lat and lng and adding label property to each object
-      markerList = res.map(({lat, lng}, index) => ({lat, lng, label: (index + 1).toString()}));
-      console.log(markerList);
-      markerList.map(item => {
-        const marker = addMarker(item);
-        return bounds.extend(marker.position);
-      });
-      // map to contain all the markers
-      googleMap.fitBounds(bounds);
-    });
-  }, []);
+  console.log(locations)
 
   return (
-    <div class='locations-wrapper'>
-      <div class='row'>
-        <div class='column locations-map'>
-          <li className="column__item">
-            <ul className="card__list">
-              <li className="card__item">
-              <div 
-                ref={googleMapRef}
-                className="google-map-locations"
-                />
-              </li>
-            </ul>
-          </li>
-        </div>
-        <div class='column locations-list'>
-          <li className="column__item">
-            <ul className="card__list">
-              <li className="card__item">
-                <span>item 1</span>
-              </li>
-              <li className="card__item">
-                <span>item 2</span>
-              </li>
-            </ul>
-          </li>
-        </div>
-      </div>
-    </div>
-  );
+    <ul className="card__list">
+      <li className="card__item">
+        <span>{locations[0].label} : {locations[0].suggestion}, {locations[0].address}</span>
+      </li>
+      <li className="card__item">
+        <span>item 2</span>
+      </li>
+    </ul>
+  )
 };
 
 export default Locations;
--- src/components/MapPage/MapPlaces.js
+++ src/components/MapPage/MapPlaces.js
@@ -1,11 +1,19 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {  faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import Map from "./Map";
 import Locations from './Locations';
+import getGeocodes from '../../requests/getGeocodes';
 import { useEventContext } from "../../hooks/useEventContext";
 
+import "../../styles/map-places.css";
+
 const MapPlaces = () => {
+  const [markerList, setMarkerList] = useState([]);
+  const [locationList, setLocationList] = useState([]);
+
   const { event } = useEventContext();
   console.log(event)
 
@@ -14,6 +22,57 @@ const MapPlaces = () => {
   const changeLocation = (redirect) => {
     navigate(redirect, { replace: true });
   };
+
+  // ordering suggested places so that they keep their indices consistently throughout
+  // containing places details such as suggestion and place_id
+  const orderedSugList = Object.values(event.Suggestions).sort((a, b) => {
+    if (a.suggestion < b.suggestion) {
+      return -1;
+    }
+    if (a.suggestion > b.suggestion) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log(orderedSugList)
+
+  // From orderedSugList, make a list of place_id
+  // const placeIdList = orderedSugList.map((suggestion) => suggestion.place_id);
+  // list containing place name and id
+  // const placeList = orderedSugList.map(({place_id, suggestion}) => ({place_id, suggestion}));
+
+  // const geocoder = new window.google.maps.Geocoder();
+  
+  // defining function for reverse geocodeing
+  // const geocoderResult = (placeId) => {
+  //   return new Promise((resolve, reject) => {
+  //     geocoder
+  //     .geocode({ placeId: placeId }, (results, status) => {
+  //         if (status === window.google.maps.GeocoderStatus.OK) {
+  //           return resolve({
+  //             place_id: results[0].place_id,
+  //             lat: results[0].geometry.location.lat(),
+  //             lng: results[0].geometry.location.lng(),
+  //             address: results[0].formatted_address,
+  //           })
+  //         } else {
+  //           reject(new Error("No results found"));
+  //         }
+  //       });
+  //   });
+  // };
+
+  // using above geocoderResult function, make a list of reverse geocoding results 
+  // containing locations and address
+  // const geocoderList= Promise.all(placeList.map(({place_id}) => geocoderResult(place_id).then(res => res)));
+
+  useEffect(() => {
+    getGeocodes(
+      orderedSugList,
+      setMarkerList,
+      setLocationList
+    )
+  }, []);
 
   return (
     <div className="map-places page">
@@ -31,7 +90,20 @@ const MapPlaces = () => {
         />
         &nbsp; Back to My Events
       </button>
-      <Locations places={event.Suggestions} />
+      <div className='map-places-wrapper'>
+        <div className='row'>
+          <div className='column locations-map'>
+            <li className="column__item">
+              <Map markers={markerList} />
+            </li>
+          </div>
+          <div className='column locations-list'>
+            <li className="column__item">
+              <Locations locations={locationList}/>
+            </li>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
